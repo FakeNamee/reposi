3b505910420474442c39ab2f103c473f8cc3bbfa
Roma <rprilipko3@inbox.ru>
2022-09-07 16:02:39 +0300

    Добавил условия задачи и схему

--- Konrol/Program.cs
+++ Konrol/Program.cs
@@ -1,35 +1,40 @@
-﻿static string[] GetStringsArray()
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длинна которых меньше либо равно 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. */
+
+
+static string[] GetStringsArray()
+{
+    string[] stringsArray = new string[4];
+    int finalArrayLength = 0;
+    int finalArrayIndex = 0;
+
+    for (int i = 0; i < stringsArray.Length; i++)
+    {
+        Console.WriteLine("Введите строку: ");
+        stringsArray[i] = Console.ReadLine();
+
+        if (stringsArray[i].Length <= 3)
         {
-            string[] stringsArray = new string[4];
-            int finalArrayLength = 0;
-            int finalArrayIndex = 0;
-
-            for (int i = 0; i < stringsArray.Length; i++)
-            {
-                Console.WriteLine("Введите строку: ");
-                stringsArray[i] = Console.ReadLine();
-
-                if (stringsArray[i].Length <= 3)
-                {
-                    finalArrayLength++;
-                }
-            }
-
-            string[] finalStringArray = new string[finalArrayLength];
-
-            foreach(string str in stringsArray)
-            {
-                if (str.Length <= 3)
-                {
-                    finalStringArray[finalArrayIndex++] = str;
-                }
-            }
-
-            return finalStringArray;
-            
+            finalArrayLength++;
         }
-       string[] Array = GetStringsArray();
-       foreach(string str in Array)
-       {
-        Console.Write(str);
-       }
+    }
+
+    string[] finalStringArray = new string[finalArrayLength];
+
+    foreach (string str in stringsArray)
+    {
+        if (str.Length <= 3)
+        {
+            finalStringArray[finalArrayIndex++] = str;
+        }
+    }
+
+    return finalStringArray;
+
+}
+string[] Array = GetStringsArray();
+foreach (string str in Array)
+{
+    Console.Write(str);
+}
